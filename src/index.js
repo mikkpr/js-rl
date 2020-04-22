10da3e9fed344f4e0b01fffe1e628ecbb5516819
Mikk Pristavka <mikk@ikald.us>
2020-04-22 17:47:52 +0300

    Add airbnb's eslint preset

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const LOG = (...msgs) => {
-    const formatMsg = msg => {
+    const formatMsg = (msg) => {
         if (typeof msg === 'object') {
             return JSON.stringify(msg, undefined, 2);
         }
         return msg;
     };
     const logEl = document.querySelector('.main__log');
-    const getRowEl = msg => {
+    const getRowEl = (msg) => {
         const el = document.createElement('div');
         el.classList.add('main__log-row');
+
         const formattedMsg = formatMsg(msg);
         el.innerHTML = formattedMsg;
 
@@ -17,7 +18,7 @@ const LOG = (...msgs) => {
 
     const rows = msgs.map(getRowEl);
 
-    rows.forEach(row => logEl.appendChild(row));
+    rows.forEach((row) => logEl.appendChild(row));
 };
 
 const setup = () => {
