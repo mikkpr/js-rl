5b088ef3cf457ad851bf9d4aaf3d18ca2f3373a7
Mikk Pristavka <mikk.pristavka@reach-u.com>
2020-05-24 23:23:18 +0300

    Add a bunch of boilerplate

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import './assets/VGA8x16.png';
+import './assets/ibm_vga8.eot';
+import './assets/ibm_vga8.woff';
+import './assets/ibm_vga8.woff2';
+import './assets/ibm_vga8.ttf';
+import './assets/main.css';
+
+const main = () => {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,57 @@
+const path = require('path');
+
 module.exports = {
-    entry: __dirname + '/src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/dist/',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
-    }
+  devtool: 'inline-source-map',
+  entry: __dirname + '/src/index.ts',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ],
+      }, {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'styles/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 };
